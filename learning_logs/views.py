6dfc9c09623b3036b381936c5fc83a86dbce238b
ratamon-tech <ratamon91@gmail.com>
2022-01-07 13:17:51 +0900

    新しいトピックを追加する

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -19,3 +20,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """新規トピックを追加する"""
+    if request.method != 'POST':
+        # データは送信されていないので空のフォームを生成する
+        form = TopicForm()
+    else:
+        # POSTでデータが送信されたのでこれを処理する
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # 空または無効のフォームを表示する
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
