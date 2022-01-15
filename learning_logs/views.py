99dd7fb51758f74d8de353df16ee251731b7c404
ratamon-tech <ratamon91@gmail.com>
2022-01-15 14:25:10 +0900

    新規トピックを現在のユーザーと紐付ける

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -39,7 +39,9 @@ def new_topic(request):
         # POSTでデータが送信されたのでこれを処理する
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # 空または無効のフォームを表示する
