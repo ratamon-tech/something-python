0071d0ece4840bf69e4f2eee70984bddf669dd24
ratamon-tech <ratamon91@gmail.com>
2022-01-15 14:17:08 +0900

    ユーザーのトピックを保護

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,7 +13,7 @@ def index(request):
 @login_required
 def topics(request):
     """全てのトピックを表示する"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -20,6 +21,10 @@ def topics(request):
 def topic(request, topic_id):
     """一つのトピックとそれについての全ての記事を表示"""
     topic = Topic.objects.get(id=topic_id)
+    # トピックが現在のユーザーが所持するものであることを確認する
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
